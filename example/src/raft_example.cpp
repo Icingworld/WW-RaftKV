81eeb2617b81f7db2e8cd12acbec524a5920d95e
Icingworld <984442022@qq.com>
2025-05-22 17:29:49 +0800

    test: modify example, use 3 nodes for simulation

--- example/src/raft_example.cpp
+++ example/src/raft_example.cpp
@@ -1,17 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+
 #include <Raft.h>
 
-int main()
+int main(int argc, char ** argv)
 {
+    if (argc < 2) {
+        std::cerr << "Too few parameters" << std::endl;
+        return -1;
+    }
+
+    if (argc > 2) {
+        std::cerr << "Too more parameters" << std::endl;
+        return -1;
+    }
+
+    int node = std::stoi(argv[1]);
+
     // 创建一些节点
     std::vector<WW::RaftPeer> peers;
     peers.emplace_back(0, "127.0.0.1", "4396");
     peers.emplace_back(1, "127.0.0.1", "4397");
     peers.emplace_back(2, "127.0.0.1", "4398");
-    peers.emplace_back(3, "127.0.0.1", "4399");
-    peers.emplace_back(4, "127.0.0.1", "4400");
 
     // 创建 Raft
-    WW::Raft raft(0, peers);
+    WW::Raft raft(node, peers);
 
     // 启动 Raft
     raft.run();
