8c92606d50eaef464c91006832c0239468db9b04
Icingworld <984442022@qq.com>
2025-05-22 14:18:07 +0800

    fix: fix a bug in closure

--- raftkv/raft-core/rpc/client/include/RaftClosure.h
+++ raftkv/raft-core/rpc/client/include/RaftClosure.h
@@ -14,7 +14,7 @@ template <typename ResponseType>
 class RaftClosure : public google::protobuf::Closure
 {
 private:
-    ResponseType _Response;
+    ResponseType * _Response;
     std::function<void(const ResponseType &)> _Callback;
 
 public:
@@ -30,7 +30,7 @@ public:
     void Run() override
     {
         if (_Callback != nullptr) {
-            _Callback(_Response);
+            _Callback(*_Response);
         }
 
         delete this;
