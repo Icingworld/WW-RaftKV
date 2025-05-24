d9fa9f2630899976052e377c36a11c9d649805d2
Icingworld <984442022@qq.com>
2025-05-24 16:12:29 +0800

    feat: create peer for raft

---
+++ raftkv/raft-core/include/RaftPeer.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <string>
+
+#include <Common.h>
+
+namespace WW
+{
+
+/**
+ * @brief 同伴
+*/
+class RaftPeer
+{
+private:
+    NodeId _Id;             // 节点 ID
+    LogIndex _Next_index;   // 应该发送的下一个日志索引
+    LogIndex _Match_index;  // 对方已确认的最后日志索引
+
+public:
+    RaftPeer(NodeId _Id);
+
+public:
+    /**
+     * @brief 获取节点 ID
+    */
+    NodeId getId() const;
+
+    /**
+     * @brief 获取应该发送的下一个日志索引
+    */
+    LogIndex getNextIndex() const;
+
+    /**
+     * @brief 获取对方已确认的最后日志索引
+    */
+    LogIndex getMatchIndex() const;
+
+    /**
+     * @brief 设置应该发送的下一个日志索引
+    */
+    void setNextIndex(LogIndex _Next_index);
+
+    /**
+     * @brief 设置对方已确认的最后日志索引
+    */
+    void setMatchIndex(LogIndex _Match_index);
+};
+
+} // namespace WW
