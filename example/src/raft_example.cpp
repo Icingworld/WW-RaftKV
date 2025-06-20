3c2f2ddd58c49133a67c71a4e9f9333630efb324
Icingworld <984442022@qq.com>
2025-06-20 20:50:56 +0800

    test: add signal handle to raft_example

--- example/src/raft_example.cpp
+++ example/src/raft_example.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
+#include <signal.h>
 
 #include <RaftClerk.h>
 
+WW::RaftClerk * raft_clerk = nullptr;
+
+void signalHandle(int sig)
+{
+    if (raft_clerk != nullptr) {
+        raft_clerk->stop();
+    }
+}
+
 int main(int argc, char ** argv)
 {
+    signal(SIGINT, signalHandle);
+    signal(SIGTERM, signalHandle);
+
     if (argc != 2) {
-        printf("param num error\n");
+        std::cerr << "param num error" << std::endl;
+        return -1;
     }
 
-    char * node_id = argv[1];
+    char * arg = argv[1];
+    WW::NodeId node_id = std::stoi(arg);
+
+    if (node_id < 0 || node_id > 6) {
+        std::cerr << "invalid node id" << std::endl;
+        return -1;
+    }
 
     // 创建 5 个节点
     std::vector<WW::RaftPeerNet> peers;
@@ -18,7 +38,7 @@ int main(int argc, char ** argv)
     }
 
     // 创建 Raft
-    WW::RaftClerk raft(std::stoi(node_id), peers);
+    raft_clerk = new WW::RaftClerk(node_id, peers);
 
-    raft.start();
+    raft_clerk->start();
 }
